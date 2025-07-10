6b20bd9495320376b777c0b4eb78b20f548a3edc
hsunr <hsunr@users.noreply.github.com>
2025-07-10 15:03:47 +0800

    用户模块引入了nacos作为配置中心和服务注册中心

--- Intelli-Edu-user/src/main/java/com/gpnu/user/IntelliEduUserApplication.java
+++ Intelli-Edu-user/src/main/java/com/gpnu/user/IntelliEduUserApplication.java
@@ -3,11 +3,13 @@ package com.gpnu.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.gpnu.**.mapper")
 @ComponentScan(basePackages = {"com.gpnu.user", "com.gpnu.common"})
+@EnableDiscoveryClient
 public class IntelliEduUserApplication {
 
     public static void main(String[] args) {
