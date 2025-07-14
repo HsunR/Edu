72932d5a334b879a06648a2e7f450b068493a476
hsunr <hsunr@users.noreply.github.com>
2025-07-14 12:23:19 +0800

    deleted:    src/api/login.js 	deleted:    src/api/menu.js 	deleted:    src/api/resource.js 	deleted:    src/api/system/dept.js 	deleted:    src/api/system/menu.js 	deleted:    src/api/system/notice.js 	deleted:    src/api/system/role.js 	deleted:    src/api/system/user.js 	deleted:    src/api/user.js 	new file:   src/temp/index.ts 	new file:   src/temp/login.ts 	new file:   src/temp/resource.ts 	new file:   src/temp/typings.d.ts 	new file:   src/temp/user.ts
    	modified:   package.json
    	modified:   src/layout/components/Navbar.vue
    	modified:   src/store/modules/permission.js
    	modified:   src/store/modules/user.js
    	modified:   src/temp/index.ts
    	modified:   src/temp/login.ts
    	modified:   src/temp/resource.ts
    	modified:   src/temp/typings.d.ts
    	modified:   src/temp/user.ts
    	modified:   src/views/loginNew.vue
    	modified:   src/views/system/user/profile/resetPwd.vue
    	modified:   src/views/system/user/profile/userAvatar.vue
    	modified:   src/views/system/user/profile/userInfo.vue
    	modified:   vite.config.js
    
    	openapi.config.js
    	src/services/

---
+++ src/temp/typings.d.ts
@@ -0,0 +1,135 @@
+declare namespace API {
+  type BaseResponseBoolean = {
+    code?: number;
+    data?: boolean;
+    message?: string;
+  };
+
+  type BaseResponseLoginResult = {
+    code?: number;
+    data?: LoginResult;
+    message?: string;
+  };
+
+  type BaseResponsePageUsUserVO = {
+    code?: number;
+    data?: PageUsUserVO;
+    message?: string;
+  };
+
+  type BaseResponseUsUserVO = {
+    code?: number;
+    data?: UsUserVO;
+    message?: string;
+  };
+
+  type getUserInfoParams = {
+    userId: number;
+  };
+
+  type LoginRequest = {
+    loginType: number;
+    username: string;
+    password: string;
+    mobile: string;
+    email: string;
+    code: string;
+    openId: string;
+  };
+
+  type LoginResult = {
+    userId?: number;
+    userType?: string;
+    accessToken?: string;
+    refreshToken?: string;
+  };
+
+  type OrderItem = {
+    column?: string;
+    asc?: boolean;
+  };
+
+  type PageUsUserVO = {
+    records?: UsUserVO[];
+    total?: number;
+    size?: number;
+    current?: number;
+    orders?: OrderItem[];
+    optimizeCountSql?: PageUsUserVO;
+    searchCount?: PageUsUserVO;
+    optimizeJoinOfCountSql?: boolean;
+    maxLimit?: number;
+    countId?: string;
+    pages?: number;
+  };
+
+  type refreshTokenParams = {
+    refreshToken: string;
+  };
+
+  type RegisterRequest = {
+    name: string;
+    password: string;
+    registerType: number;
+    mobile: string;
+    email: string;
+    code: string;
+  };
+
+  type SendLoginCodeRequest = {
+    loginType: number;
+    mobile: string;
+    email: string;
+  };
+
+  type SendRegisterCodeRequest = {
+    registerType: number;
+    mobile: string;
+    email: string;
+  };
+
+  type UsUserDeleteRequest = {
+    userId?: number;
+  };
+
+  type UsUserQueryRequest = {
+    current?: number;
+    pageSize?: number;
+    sortField?: string;
+    sortOrder?: string;
+    userId?: number;
+    name?: string;
+    password?: string;
+    type?: string;
+    sex?: number;
+    email?: string;
+    mobile?: string;
+    school?: string;
+  };
+
+  type UsUserUpdateRequest = {
+    userId?: number;
+    name?: string;
+    password?: string;
+    sex?: number;
+    headPortrait?: string;
+    personalSignature?: string;
+    school?: string;
+  };
+
+  type UsUserVO = {
+    userId?: number;
+    name?: string;
+    password?: string;
+    type?: string;
+    openId?: string;
+    sex?: number;
+    email?: string;
+    mobile?: string;
+    headPortrait?: string;
+    personalSignature?: string;
+    school?: string;
+    createTime?: string;
+    updateTime?: string;
+  };
+}
---
+++ src/temp/user.ts
@@ -0,0 +1,63 @@
+// @ts-ignore
+/* eslint-disable */
+import request from "../utils/request";
+
+/** 删除用户 根据用户ID删除用户 POST /user/deleteUser */
+export async function deleteUser(
+  body: API.UsUserDeleteRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean>("/user/deleteUser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 获取用户信息 根据用户ID获取用户信息 GET /user/getUserInfo */
+export async function getUserInfo(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getUserInfoParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseUsUserVO>("/user/getUserInfo", {
+    method: "GET",
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** 多条件查询 多条件查询 POST /user/list */
+export async function listUsers(
+  body: API.UsUserQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageUsUserVO>("/user/list", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 更新用户信息 根据用户ID更新用户信息 POST /user/update */
+export async function updateUserInfo(
+  body: API.UsUserUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseUsUserVO>("/user/update", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
