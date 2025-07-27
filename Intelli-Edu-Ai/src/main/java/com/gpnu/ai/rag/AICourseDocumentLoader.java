bf9bb89c59e1401159b64c3884641fa5139cfaa5
hsunr <hsunr@users.noreply.github.com>
2025-07-27 13:18:51 +0800

    新增了Tika文档读取（不能解析PDF，因为没有OCR识别支持）

--- Intelli-Edu-Ai/src/main/java/com/gpnu/ai/rag/AICourseDocumentLoader.java
+++ Intelli-Edu-Ai/src/main/java/com/gpnu/ai/rag/AICourseDocumentLoader.java
@@ -1,6 +1,9 @@
 package com.gpnu.ai.rag;
 
 
+import com.gpnu.ai.wrapper.CustomTikaDocumentReader;
+import com.gpnu.common.exception.ErrorCode;
+import com.gpnu.common.exception.ThrowUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
@@ -10,9 +13,12 @@ import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -26,40 +32,8 @@ public class AICourseDocumentLoader {
     }
 
 
-    public List<Document> loadPdfByTika(){
-        List<Document> documents = new ArrayList<>();
-        //加载pdf文件
-        try{
-           Resource [] resources = resourcePatternResolver.getResources("classpath:document/*.pdf");
-            for(Resource resource:resources){
-                String filename = resource.getFilename();
-                TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(resource);
-                log.info(tikaDocumentReader .get().toString());
-                documents.addAll(tikaDocumentReader.get());
-            }
-        }catch (IOException e){
-            log.error("加载pdf文件失败",e);
-        }
-        return documents;
-    }
-
-    public List<Document> loadPdfByPdfReader(){
-        List<Document> documents = new ArrayList<>();
-        //加载pdf文件
-        try {
-            Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.pdf");
-            for (Resource resource : resources){
-                String filename = resource.getFilename();
-                PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(resource);
-                documents.addAll(pdfReader.get());
-            }
-        }catch (IOException e){
-            log.error("加载pdf文件失败",e);
-        }
-        return documents;
-    }
     /**
-     * 加载本地的markdown文档
+     * 加载本地的markdown文档（用来读取公共文档），不再使用
      * @return
      */
     public List<Document> loadMarkdowns(){
@@ -71,7 +45,7 @@ public class AICourseDocumentLoader {
                 String filename = resource.getFilename();
                 //提前文档倒数第三个和第二个字作为标签
                 String status = "AI通识历史";
-                String tag = "AI通识历史,深度学习，人工智能";
+                String tag = "AI通识历史;深度学习;人工智能";
                 MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
                         .withHorizontalRuleCreateDocument(true)
                         .withIncludeCodeBlock(false)
@@ -79,6 +53,7 @@ public class AICourseDocumentLoader {
                         .withAdditionalMetadata("filename", filename)
                         .withAdditionalMetadata("status", status)
                         .withAdditionalMetadata("tag", tag)
+                        .withAdditionalMetadata("accessPermission","public")
                         .build();
                 MarkdownDocumentReader markdownDocumentReader = new MarkdownDocumentReader(resource, config);
                 allDocuments.addAll(markdownDocumentReader.get());
@@ -88,5 +63,16 @@ public class AICourseDocumentLoader {
         }
         return allDocuments;
     }
+
+    public List<Document> parsePublicDocumentByTika(MultipartFile multipartFile, Map<String, Object> customMetadata) {
+         List<Document> documents = new ArrayList<>();
+         Resource resource = multipartFile.getResource();
+         ThrowUtils.throwIf(!resource.exists(), ErrorCode.OPERATION_ERROR,"上传文件不存在");
+         CustomTikaDocumentReader tikaDocumentReader = new CustomTikaDocumentReader(resource, customMetadata);
+         documents.addAll(tikaDocumentReader.get());
+        return documents;
+
+    }
+
 
 }
